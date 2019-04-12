dadb63d377ad95aa24a9640f0ae3c8ce6fa0695f
s.peshkur <s.peshkur@severex.io>
2019-04-12 12:09:01 +0300

    Additn data to the spring model

--- src/main/java/by/peshkur/mvc/springboot/controllers/HelloWorldController.java
+++ src/main/java/by/peshkur/mvc/springboot/controllers/HelloWorldController.java
@@ -1,8 +1,36 @@
 package by.peshkur.mvc.springboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        //read request from the HTML form
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! " + theName;
+
+        //add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 
 }
