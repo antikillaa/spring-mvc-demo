c8240b8c3cfa0ac61f76c477be40b8a503ff2984
s.peshkur <s.peshkur@severex.io>
2019-04-12 12:34:07 +0300

    Request param

--- src/main/java/by/peshkur/mvc/springboot/controllers/HelloWorldController.java
+++ src/main/java/by/peshkur/mvc/springboot/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package by.peshkur.mvc.springboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,6 +27,21 @@ public class HelloWorldController {
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "Yo! " + theName;
+
+        //add message to model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(
+            //request param
+            @RequestParam("studentName") String theName,
+            Model model) {
+
+        theName = theName.toUpperCase();
+        String result = "Hey My Friend from v3! " + theName;
 
         //add message to model
         model.addAttribute("message", result);
