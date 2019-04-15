e7d7fb60c51581e30a9723e0be9cf7e4ca98edca
s.peshkur <s.peshkur@severex.io>
2019-04-15 14:00:02 +0300

    select from java class

--- src/main/java/by/peshkur/mvc/springboot/student/model/Student.java
+++ src/main/java/by/peshkur/mvc/springboot/student/model/Student.java
@@ -1,13 +1,23 @@
 package by.peshkur.mvc.springboot.student.model;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
 
-    public Student() {
+    private LinkedHashMap<String, String> countryOptions;
 
+    public Student() {
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+        countryOptions.put("US", "USA");
     }
 
     public String getFirstName() {
@@ -33,4 +43,9 @@ public class Student {
     public void setCountry(String country) {
         this.country = country;
     }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
 }
