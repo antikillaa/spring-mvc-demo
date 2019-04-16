10b9fe541d6b35872e7bd2700508128932e9f3d7
s.peshkur <s.peshkur@severex.io>
2019-04-16 15:39:53 +0300

    integer required  validation

--- src/main/java/by/peshkur/mvc/springboot/customer/model/Customer.java
+++ src/main/java/by/peshkur/mvc/springboot/customer/model/Customer.java
@@ -1,9 +1,6 @@
 package by.peshkur.mvc.springboot.customer.model;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -12,9 +9,13 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than or or equal to zero")
     @Max(value = 10, message = "must be less than or or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public Customer() {
     }
@@ -35,11 +36,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
